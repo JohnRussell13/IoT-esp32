5beee7084d3bcc9481b34d19f83cfaf256575e87
JohnRussell13 <aleksandar.g1999@gmail.com>
2022-09-02 13:37:07 +0200

    Plotter

--- Plotter/Plotter.ino
+++ Plotter/Plotter.ino
@@ -4,24 +4,19 @@
 #define PIN_DHT11 13 // Moisture/Temp sensor DHT11 connected to PIN A1
 
 dht DHT;
+unsigned int AnalogValue;
 
 void setup(){
   Serial.begin(9600);
+  Serial.println("Light[%] Humidity[%] Temperature[C]");
 }
 
 void loop(){
-  unsigned int AnalogValue;
   AnalogValue = analogRead(PIN_PHOTO);
-  Serial.print("Current light intesity = ");
-  Serial.print(AnalogValue);
-  
   DHT.read11(PIN_DHT11);
-  Serial.print("  humidity = ");
+  Serial.print((float(4095-AnalogValue))/4095*100);
+  Serial.print(",");
   Serial.print(DHT.humidity);
-  Serial.print("%");
-  Serial.print("  temperature = ");
-  Serial.print(DHT.temperature); 
-  Serial.println("C");
-
-  delay(1000);
+  Serial.print(",");
+  Serial.println(DHT.temperature);
 }
