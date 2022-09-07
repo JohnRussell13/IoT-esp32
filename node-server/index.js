84ee5f608614cf726c512c6682abbdfc2bf0b4eb
JohnRussell13 <aleksandar.g1999@gmail.com>
2022-09-07 11:20:48 +0200

    Fixes

--- node-server/index.js
+++ node-server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', sensorRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 const HOST = '0.0.0.0'
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
